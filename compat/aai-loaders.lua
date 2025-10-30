b8a34b8958fcbcaa43ddd55ebf24c5790a09f990
Syen-ce <angellecce2003@gmail.com>
2025-10-30 11:39:55 +0100

    Fixed crash with aai loaders on graphic only mode

--- compat/aai-loaders.lua
+++ compat/aai-loaders.lua
@@ -1,3 +1,3 @@
-if not mods["aai-loaders"] then return end
+if not mods["aai-loaders"] and settings.startup["aai-loaders-mode"].value ~= "graphics-only" then return end
 
 data.raw["item"]["aai-hyper-loader"].default_import_location = "hyarion"
