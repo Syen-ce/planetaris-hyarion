a25e19414df07dfbfb5690153ef366d1331d35b9
Syen-ce <angellecce2003@gmail.com>
2026-03-16 12:08:32 +0100

    1.2.8

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,6 +1,5 @@
 require("compat.aai-loaders")
 require("compat.arig")
 require("compat.celestial-weather")
-require("compat.k2so")
 
 data.raw["assembling-machine"]["planetaris-assembling-machine-4"].crafting_categories = data.raw["assembling-machine"]["assembling-machine-3"].crafting_categories
