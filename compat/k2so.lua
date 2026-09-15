6f1b40c89f799ab501558c002a35bc8bca405d21
Syen-ce <angellecce2003@gmail.com>
2026-09-16 01:01:40 +0200

    1.3.23

--- compat/k2so.lua
+++ compat/k2so.lua
@@ -216,6 +216,35 @@ data.extend({
     }
 })
 
+if not mods["planetaris-arig"] then
+    data.extend({
+        {
+        type = "recipe",
+        name = "planetaris-lig-carbon-wood-chemistry",
+        categories = {"chemistry"},
+        enabled = false,
+        auto_recycle = false,
+        energy_required = 1,
+        icon = "__base__/graphics/icons/wood.png",
+        localised_name = {"", {"item-name.wood"}, " (", {"space-location-name.hyarion"}, ")"},
+        ingredients =
+        {
+        {type = "item", name = "carbon", amount = 2},
+        },
+        results = {{type="item", name="wood", amount=5}},
+        allow_productivity = false
+        },
+    })
+
+    table.insert(data.raw["technology"]["planetaris-metallic-ore-processing"].effects,
+        {
+          type = "unlock-recipe",
+          recipe = "planetaris-lig-carbon-wood-chemistry"
+        }
+      )
+
+end
+
 data_util.add_recipe_unlock("planetaris-metallic-ore-processing", "planetaris-rare-ore-metallic-separation")
 
 end
