813501d2f7755ef1e05927664dfea2a84b5282c8
Syen-ce <angellecce2003@gmail.com>
2026-09-16 08:30:09 +0200

    Update changelog

--- compat/k2so.lua
+++ compat/k2so.lua
@@ -183,11 +183,29 @@ data.extend({
       allow_productivity = true,
       crafting_machine_tint = {primary = {0.57, 0.55, 0.969, 1}, secondary = {0.57, 0.55, 0.969, 1}},
     },
+    {
+      type = "recipe",
+      name = "planetaris-lig-carbon-wood-chemistry",
+      categories = {"sifting"},
+      enabled = false,
+      auto_recycle = false,
+      energy_required = 1,
+      icon = "__base__/graphics/icons/wood.png",
+      localised_name = {"", {"item-name.wood"}, " (", {"space-location-name.hyarion"}, ")"},
+      ingredients =
+      {
+        {type = "item", name = "carbon", amount = 2},
+      },
+      results = {{type="item", name="wood", amount=5}},
+      allow_productivity = false
+    },
 })
 
-data_util.add_recipe_unlock("planetaris-metallic-ore-processing", "planetaris-rare-ore-metallic-sifting")
+PlanetarisLib.add_tech_unlock("planetaris-metallic-ore-processing", "planetaris-lig-carbon-wood-chemistry")
+PlanetarisLib.add_tech_unlock("planetaris-metallic-ore-processing", "planetaris-rare-ore-metallic-sifting")
 
 else
+  
 data.extend({
     {
       type = "recipe",
@@ -213,12 +231,8 @@ data.extend({
 
       allow_productivity = true,
       crafting_machine_tint = {primary = {0.57, 0.55, 0.969, 1}, secondary = {0.57, 0.55, 0.969, 1}},
-    }
-})
-
-if not mods["planetaris-arig"] then
-    data.extend({
-        {
+    },
+    {
         type = "recipe",
         name = "planetaris-lig-carbon-wood-chemistry",
         categories = {"chemistry"},
@@ -233,18 +247,10 @@ if not mods["planetaris-arig"] then
         },
         results = {{type="item", name="wood", amount=5}},
         allow_productivity = false
-        },
-    })
+      },
+})
 
-    table.insert(data.raw["technology"]["planetaris-metallic-ore-processing"].effects,
-        {
-          type = "unlock-recipe",
-          recipe = "planetaris-lig-carbon-wood-chemistry"
-        }
-      )
-
-end
-
-data_util.add_recipe_unlock("planetaris-metallic-ore-processing", "planetaris-rare-ore-metallic-separation")
+PlanetarisLib.add_tech_unlock("planetaris-metallic-ore-processing", "planetaris-lig-carbon-wood-chemistry")
+PlanetarisLib.add_tech_unlock("planetaris-metallic-ore-processing", "planetaris-rare-ore-metallic-separation")
 
 end
--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -9,4 +9,6 @@ require("compat.lamp-post")
 require("compat.muluna")
 require("compat.science-group")
 
-data.raw["assembling-machine"]["planetaris-assembling-machine-4"].crafting_categories = data.raw["assembling-machine"]["assembling-machine-3"].crafting_categories
+if data.raw["assembling-machine"]["planetaris-assembling-machine-4"] then
+    data.raw["assembling-machine"]["planetaris-assembling-machine-4"].crafting_categories = data.raw["assembling-machine"]["assembling-machine-3"].crafting_categories
+end
